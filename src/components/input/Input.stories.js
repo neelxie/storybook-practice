193c562ba12268359978c2a4d358bbd0ba18b262
neelxie <kidricederek@gmail.com>
2022-03-06 04:16:06 +0300

    Add input story

---
+++ src/components/input/Input.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Input from "./Input";
+
+export default {
+  title: 'Input',
+  component: Input
+}
+
+export const Small = () => <Input size='small'/>
+export const Medium = () => <Input size='medium'/>
+export const Large = () => <Input size='large'/>
