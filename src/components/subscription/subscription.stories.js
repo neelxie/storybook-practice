f2d86b0e8460331f07186c31ca4e90b0c43b887a
neelxie <kidricederek@gmail.com>
2022-03-06 05:17:02 +0300

    Story within a story

---
+++ src/components/subscription/subscription.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Primary } from "../button/Button.stories";
+import { Large } from "../input/Input.stories";
+
+export default {
+  title: "Form/subscription",
+};
+
+export const PrimarySubscription = () => {
+  <>
+    <Large />
+    <Primary />
+  </>;
+};
